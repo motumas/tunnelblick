motumas/tunnelblick

commit f89c442954d2c4e53192fe690f9baa369d87fe1b

Build 3433: Two changes: Detects and complains about rich-text config and other files when installing a .tblk or converting an OpenVPN configuration, and fixes a problem that caused an extra dialog window to appear when cancelling the installation of a .tblk
git-svn-id: http://tunnelblick.googlecode.com/svn/trunk@2433 ecaf4b9d-c145-0410-a236-1b700048ef5c

tunnelblick/ConfigurationManager.m
@@ -652,7 +652,8 @@ enum state_t {                      // These are the "states" of the guideState 
     
     // List of OpenVPN options that take a file path
     NSArray * optionsWithPath = [NSArray arrayWithObjects:
-	//					         @"askpass",                       // askpass 'file' not supported since we don't compile with --enable-password-save
+								 // @"askpass",                    // askpass        'file' not supported since we don't compile with --enable-password-save
+								 // @"auth-user-pass",             // auth-user-pass 'file' not supported since we don't compile with --enable-password-save
 								 @"ca",
 								 @"cert",
 								 @"dh",
@@ -686,7 +687,14 @@ enum state_t {                      // These are the "states" of the guideState 
                 return FALSE;
             }
             if (  ! [argument isEqualToString: @"[inline]"]  ) {
-                if (  ! [gFileMgr fileExistsAtPath: [[cfgPath stringByDeletingLastPathComponent] stringByAppendingPathComponent: argument]]  ) {
+                NSString * newPath = [[cfgPath stringByDeletingLastPathComponent] stringByAppendingPathComponent: argument];
+                if (  [gFileMgr fileExistsAtPath: newPath]  ) {
+                    NSString * errorText = errorIfNotPlainTextFileAtPath(newPath, YES);
+                    if (  errorText  ) {
+                        NSLog(@"Error in %@ (referenced in %@): %@", [newPath lastPathComponent], tblkName, errorText);
+                        return FALSE;
+                    }
+                } else {
                     NSLog(@"The configuration file in %@ has a '%@' option with file '%@' which cannot be found.",
                           tblkName, option, argument);
                     TBRunAlertPanel(NSLocalizedString(@"Tunnelblick VPN Configuration Installation Error", @"Window title"),
@@ -862,9 +870,13 @@ enum state_t {                      // These are the "states" of the guideState 
                             : NSLocalizedString(@"There was a problem with one or more configurations. Details are in the Console Log.\n\n", @"Window text")
                             );
     
-    if (   ([errList    count] != 0)
-        && ([deleteList count] == 0)
+    if (   ([deleteList count] == 0)
         && ([sourceList count] == 0)  ) {
+        
+        if ( [errList   count] == 0  ) {
+            return;		// The user cancelled
+        }
+        
         TBRunAlertPanel(NSLocalizedString(@"Tunnelblick VPN Configuration Installation Error", @"Window title"),
                         errPrefix,
                         nil, nil, nil);
@@ -1214,6 +1226,22 @@ enum state_t {                      // These are the "states" of the guideState 
         && ( ! pkgDoUninstall)
         && [self isSampleConfigurationAtPath: pathToConfigFile]  ) {
         pkgIsOK = FALSE;            // have already informed the user of the problem
+    }
+    
+    // **************************************************************************************
+    // Make sure the configuration file is a plain text file
+    NSString * errorText = errorIfNotPlainTextFileAtPath(pathToConfigFile, YES);
+    if (  errorText  ) {
+		NSString * tblkName = [pathToConfigFile stringByDeletingLastPathComponent];	// Remove config.ovpn
+        if (   [[tblkName lastPathComponent] isEqualToString: @"Resources"]  ) {    // Remove Contents/Resources
+            tblkName = [tblkName stringByDeletingLastPathComponent];
+            if (   [[tblkName lastPathComponent] isEqualToString: @"Contents"]  ) {
+                tblkName = [tblkName stringByDeletingLastPathComponent];
+            }
+        }
+        tblkName = [tblkName lastPathComponent];									// Isolate whatever.tblk
+		NSLog(@"Error in configuration file in %@: %@", tblkName, errorText);
+        return nil;
     }
     
     // **************************************************************************************

tunnelblick/sharedRoutines.m
@@ -418,3 +418,41 @@ BOOL secureOneFolder(NSString * path, BOOL isPrivate, uid_t theUser)
     
 	return result;
 }
+
+NSString * errorIfNotPlainTextFileAtPath(NSString * path, BOOL crIsOK) {
+    
+    // Returns nil if the file at path is a plain text file, or a string describing the error if it isn't
+    //
+    // If crOK is TRUE, CR (0x0D) characters are allowed.
+    // Note: all files except script files can have CR characters.
+    
+    NSData * data = [[NSFileManager defaultManager] contentsAtPath: path];
+    
+    const char * chars = [data bytes];
+    
+    if (   chars[0] == '{'  ) {
+        return @"The file appears to be in \"rich text\" format because it starts with a '{' character. Configuration files and all other OpenVPN-related files must be \"plain text\" files.";
+    }
+    
+    unsigned i;
+    unsigned lineNumber = 1;
+    for (  i=0; i<[data length]; i++  ) {
+        char c = chars[i];
+        if (   ((c & 0x80) != 0)   // If high bit set
+            || (c == 0x7F)         // Or DEL
+            || (   (c == 0x0D)     // Or CR and CR is not allowed
+                && (! crIsOK))
+            || (   (c < 0x20)      // Or a control character
+                && (c != 0x09)     //    but not an HTAB
+                && (c != 0x0A)     //            or LF
+                )
+            ) {
+            return [NSString stringWithFormat: @"Line %d of the file contains a non-printable character (0x%02X) which is not allowed.", lineNumber, c];
+        }
+        if (  c == 0x0A  ) {
+            lineNumber++;
+        }
+    }
+    
+    return nil;
+}

tunnelblick/ConfigurationConverter.m
@@ -62,10 +62,10 @@ extern NSString      * gPrivatePath;
 
 -(void) logMessage: (NSString *) msg {
 	NSString * fullMsg;
-	if (  inputLineNumber != 0  ) {
-		fullMsg = [NSString stringWithFormat: @"%@ line %u: %@", [self nameToDisplayFromPath: configPath], inputLineNumber, msg];
-	} else {
+	if (  inputLineNumber == 0  ) {
 		fullMsg = [NSString stringWithFormat: @"%@: %@", [self nameToDisplayFromPath: configPath], msg];
+	} else {
+		fullMsg = [NSString stringWithFormat: @"%@ line %u: %@", [self nameToDisplayFromPath: configPath], inputLineNumber, msg];
 	}
 	if (  logFile == NULL  ) {
 		NSLog(@"%@", fullMsg);
@@ -239,6 +239,7 @@ extern NSString      * gPrivatePath;
 -(BOOL) processPathRange: (NSRange) rng
 	   removeBackslashes: (BOOL) removeBackslashes
         needsShExtension: (BOOL) needsShExtension {
+    
 	NSString * inPathString = [configString substringWithRange: rng];
 	NSString * inPath = [[inPathString copy] autorelease];
 	if (  removeBackslashes  ) {
@@ -259,6 +260,12 @@ extern NSString      * gPrivatePath;
         if (  ! [extension isEqualToString: @"sh"]  ) {
             fileWithNeededExtension = [file stringByAppendingPathExtension: @"sh"];
             [self logMessage: [NSString stringWithFormat: @"Added '.sh' extension to %@ so it will be secured properly", file]];
+        }
+        
+        NSString * errorMsg = errorIfNotPlainTextFileAtPath(inPath, NO);
+        if (  errorMsg  ) {
+            [self logMessage: [NSString stringWithFormat: @"File %@: %@", inPath, errorMsg]];
+            return FALSE;
         }
     } else {
         if (   ( ! extension)
@@ -347,9 +354,18 @@ extern NSString      * gPrivatePath;
     outputPath   = [theOutputPath copy];
     logFile      = theLogFile;
 	
+    inputIx         = 0;
+    inputLineNumber = 0;
+    
 	tokensToReplace    = [[NSMutableArray alloc] initWithCapacity: 8];
 	replacementStrings = [[NSMutableArray alloc] initWithCapacity: 8];
 	
+    NSString * errorMsg = errorIfNotPlainTextFileAtPath(theConfigPath, YES);
+    if (  errorMsg  ) {
+        [self logMessage: errorMsg];
+        return FALSE;
+    }
+    
     configString = [[[[NSString alloc] initWithContentsOfFile: configPath encoding: NSASCIIStringEncoding error: NULL] autorelease] mutableCopy];
     
     // Append newline to file if it doesn't aleady end in one (simplifies parsing)
@@ -361,8 +377,8 @@ extern NSString      * gPrivatePath;
 	
     // List of OpenVPN options that take a file path
     NSArray * optionsWithPath = [NSArray arrayWithObjects:
-//					             @"askpass",                       // askpass 'file' not supported since we don't compile with --enable-password-save
-								 @"auth-user-pass",				   // Optional 'file'
+//					             @"askpass",                       // askpass        'file' not supported since we don't compile with --enable-password-save
+//								 @"auth-user-pass",				   // auth-user-pass 'file' not supported since we don't compile with --enable-password-save
 								 @"ca",
 								 @"cert",
 								 @"dh",
@@ -416,9 +432,6 @@ extern NSString      * gPrivatePath;
                                @"</secret>",
                                @"</tls-auth>",
                                nil];
-    
-    inputIx         = 0;
-    inputLineNumber = 0;
     
     // Create the .tblk/Contents/Resources folder
     if (  outputPath  ) {

tunnelblick/sharedRoutines.h
@@ -49,3 +49,5 @@ unsigned int getFreePort(void);
 BOOL itemIsVisible(NSString * path);
 
 BOOL secureOneFolder(NSString * path, BOOL isPrivate, uid_t theUser);
+
+NSString * errorIfNotPlainTextFileAtPath(NSString * path, BOOL crIsOK);

commit 17cb2a0c325985c96a9a801f8c9a5a491da5df82

Build 3441: Fixes two problems introduced in r2433

tunnelblick/ConfigurationManager.m
@@ -1228,22 +1228,25 @@ enum state_t {                      // These are the "states" of the guideState 
         pkgIsOK = FALSE;            // have already informed the user of the problem
     }
     
-    // **************************************************************************************
-    // Make sure the configuration file is a plain text file
-    NSString * errorText = errorIfNotPlainTextFileAtPath(pathToConfigFile, YES);
-    if (  errorText  ) {
-		NSString * tblkName = [pathToConfigFile stringByDeletingLastPathComponent];	// Remove config.ovpn
-        if (   [[tblkName lastPathComponent] isEqualToString: @"Resources"]  ) {    // Remove Contents/Resources
-            tblkName = [tblkName stringByDeletingLastPathComponent];
-            if (   [[tblkName lastPathComponent] isEqualToString: @"Contents"]  ) {
-                tblkName = [tblkName stringByDeletingLastPathComponent];
-            }
-        }
-        tblkName = [tblkName lastPathComponent];									// Isolate whatever.tblk
-		NSLog(@"Error in configuration file in %@: %@", tblkName, errorText);
-        return nil;
-    }
-    
+	if (  pathToConfigFile  ) {
+		// **************************************************************************************
+		// Make sure the configuration file is a plain text file
+		NSString * errorText = errorIfNotPlainTextFileAtPath(pathToConfigFile, YES);
+		if (  errorText  ) {
+			NSString * tblkName = [pathToConfigFile stringByDeletingLastPathComponent];	// Remove config.ovpn
+			if (   [[tblkName lastPathComponent] isEqualToString: @"Resources"]  ) {    // Remove Contents/Resources
+				tblkName = [tblkName stringByDeletingLastPathComponent];
+				if (   [[tblkName lastPathComponent] isEqualToString: @"Contents"]  ) {
+					tblkName = [tblkName stringByDeletingLastPathComponent];
+				}
+			}
+			tblkName = [tblkName lastPathComponent];									// Isolate whatever.tblk
+			NSLog(@"Error in configuration file in %@: %@", tblkName, errorText);
+			return nil;
+		}
+	}
+
+	
     // **************************************************************************************
     // Make sure the .tblk contains all key/cert/etc. files that are in the configuration file
     if (   pathToConfigFile

tunnelblick/sharedRoutines.m
@@ -427,6 +427,10 @@ NSString * errorIfNotPlainTextFileAtPath(NSString * path, BOOL crIsOK) {
     // Note: all files except script files can have CR characters.
     
     NSData * data = [[NSFileManager defaultManager] contentsAtPath: path];
+	
+	if (  ! data  ) {
+		return @"The file is missing.";
+	}
     
     const char * chars = [data bytes];
     
@@ -445,6 +449,7 @@ NSString * errorIfNotPlainTextFileAtPath(NSString * path, BOOL crIsOK) {
             || (   (c < 0x20)      // Or a control character
                 && (c != 0x09)     //    but not an HTAB
                 && (c != 0x0A)     //            or LF
+                && (c != 0x0D)     //            or CR
                 )
             ) {
             return [NSString stringWithFormat: @"Line %d of the file contains a non-printable character (0x%02X) which is not allowed.", lineNumber, c];